c548132681de77eb6b30f8ee8ca18ad0b35317d6
Kadi Jairus <98668516+kadijairus@users.noreply.github.com>
2023-02-07 21:42:50 +0200

    Add files via upload
    utf-8 probleem on veel lahendamata

--- Sendmail.py
+++ Sendmail.py
@@ -9,27 +9,48 @@ import smtplib, ssl, os
 import config
 
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = config.user
-apppassword = config.password
-receiver_email = "kadijairus@gmail.com"
-variable = 5
-
-message = f"""\
-Subject: Ajakava tabelis {variable} muudatust
-
-This message is sent from Python.
-
+# https://mljar.com/blog/python-send-email/ 2023-02-07
+def mailsender(receiver_email, subject, message):
+    try:
+        sender_email = config.user
+        apppassword = config.password
+        
+        if sender_email is None or apppassword is None:
+            # no email address or password
+            # something is not configured properly
+            print("Did you set email address and password correctly?")
+            return False
+        
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        
+        message = f"""\
+Subject: {subject}
+        
+{message}
+        
 Tervitustega
-
 Meilirobot
 
 """
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, apppassword)
-    server.sendmail(sender_email, receiver_email, message)
-    
-print("Tehtud")
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, apppassword)
+            server.sendmail(sender_email, receiver_email, message)
+            print("Mailsender OK!")
+        return True
+
+    except Exception as e:
+        print(sender_email)
+        print(receiver_email)
+        print(message)
+        print("Error!")
+        print(str(e))
+    return False
+
+message1 = "Põhitekst asub siin"
+message1 = message1.encode('utf-8').decode('latin-1')
+print(message1)
+paus = 1/0
+mailsender("kadijairus@gmail.com","Teavitus",message1)
