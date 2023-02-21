f953a16f3734ff0a111d1eced287a76feb307e33
Kadi Jairus <98668516+kadijairus@users.noreply.github.com>
2023-02-21 13:19:03 +0200

    Outlook, lastchanged

--- Sendmail.py
+++ Sendmail.py
@@ -5,6 +5,9 @@
 # v2
 # Kadi Jairus
 
+
+import datetime
+import win32com.client as win32
 import pandas as pd
 import shutil
 import smtplib, ssl, os
@@ -12,78 +15,31 @@ import smtplib, ssl, os
 import config
 
 
-#dir_old = r'C:\Intel\KadiJairus\Arhiiv\2023 Ajakava.xlsx'
-#dir_original = = r'\\srvlaste\Yhendlabor\GE_Geneetikakeskus\Puhkused_koolitused\2023 Ajakava.xlsx'
-#dir_old = r'C:\Intel\KadiJairus\Arhiiv\2023 Ajakava uus.xlsx'
-# Asukohad testkaustas
-dir_old = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\Sendmail_arhiiv\2023 Ajakava.xlsx'
-dir_original = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\2023 Ajakava.xlsx'
-dir_new = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\Sendmail_arhiiv\2023 Ajakava uus.xlsx'
+# TheoretiCAL https://stackoverflow.com/questions/6332577/send-outlook-email-via-python
+def outlookmailsender(receiver_email, subject, message,lastchanged):
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = receiver_email
+    mail.Subject = subject
+    mail.Body = f"""\
+Tere!
 
-try:
-    shutil.copy(dir_original,dir_new)
-except:
-    print("Jätkan")
+Ajakavas {subject}.
 
+{message}
 
-allsheetnames = ["P","T","H","K","E","M"]
+Tabelit muudeti viimati {lastchanged}.
+        
+Tervitustega
+Meilirobot
 
-names = pd.read_excel(dir_new,sheet_name='Töötajad',header=2,usecols=[4,18])
-names.columns = ['Nimi', 'Dpt']
-names.dropna()
-names = names[names['Dpt'].str.contains("kliiniline geneetika")==True]
-names = names['Nimi']
-#names = list(names["Name"])
-print(names)
+"""
+    #mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
+    # To attach a file to the email (optional):
+    #attachment  = "Path to the attachment"
+    #mail.Attachments.Add(attachment)
+    mail.Send()
 
-
-def sheetcomparer(dir_old,dir_new,sheetname):
-    df_old = pd.read_excel(dir_old,sheet_name=sheetname,header=1,usecols=["Nimi","Algus","Lõpp"])
-    df_new = pd.read_excel(dir_new,sheet_name=sheetname,header=1,usecols=["Nimi","Algus","Lõpp"])
-    df = df_new.compare(df_old, align_axis=0, keep_shape = False).rename(index={'self': 'uus', 'other': 'enne muutmist'},level=-1)
-    df["Leht"] = sheetname
-    df.fillna('-')
-    return(df)
-
-df = []
-try:
-    df_appended=[]
-    if len(allsheetnames) > 0:
-        for asheetname in allsheetnames:
-            print(asheetname)
-            df_sheet = sheetcomparer(dir_old,dir_new,asheetname)
-            df_appended.append(df_sheet)
-            print(df_appended)
-        df = pd.concat(df_appended)
-        df['Algus'] = pd.to_datetime(df['Algus'])
-        df['Algus'] = df['Algus'].dt.strftime('%d.%m.%Y')
-        df['Lõpp'] = pd.to_datetime(df['Lõpp'])
-        df['Lõpp'] = df['Lõpp'].dt.strftime('%d.%m.%Y')
-        print('Enne filtreerimist')
-        print(df)
-        #df['Name'] =df['Nimi']
-        nimedefilter = (df['Nimi'].isin(names))
-        df = df.loc[nimedefilter]
-#        df = df.to_string(index=False)
-        print("FILTREERITUD")
-        print(df)
-    else:
-        print("No sheetnames!")
-except Exception as e:
-    print("Sheetcomparer error!")
-    print(str(e))
-
-
-if len(df) > 0:
-    subject = "on tehtud muudatusi"
-else:
-    subject = "ei ole ühtegi muudatust"
-    df = ''
-#print(df)
-
-#teesiinpaus = 1/0
-
-#shutil.copy(dir_new,dir_old)
 
 # https://mljar.com/blog/python-send-email/ 2023-02-07
 def mailsender(receiver_email, subject, message):
@@ -129,11 +85,105 @@ Meilirobot
         print(str(e))
     return False
 
+#mailsender("kadijairus@gmail.com", "Esimene def laetud","Esimene def laetud")
+
+dir_old = r'C:\Intel\KadiJairus\2023 Ajakava.xlsx'
+dir_original = r'\\srvlaste\Yhendlabor\GE_Geneetikakeskus\Puhkused_koolitused\2023 Ajakava.xlsx'
+dir_new = r'C:\Intel\KadiJairus\2023 Ajakava uus.xlsx'
+# Asukohad testkaustas
+#dir_old = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\Sendmail_arhiiv\2023 Ajakava.xlsx'
+#dir_original = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\2023 Ajakava.xlsx'
+#dir_new = r'D:\Users\loom\Desktop\Pisi\T88\Python jms\Sendmail_arhiiv\2023 Ajakava uus.xlsx'
+
+#mailsender("kadijairus@gmail.com", "Tabelite asukohad laetud","Tabelite asukohad laetud")
+
+try:
+    shutil.copy(dir_original,dir_new)
+except:
+    print("Jätkan")
+
+try:
+    oslastchanged = os.path.getmtime(dir_original)
+    lastchanged = datetime.datetime.fromtimestamp(oslastchanged).strftime("%d.%m.%Y %H.%M")
+except:
+    lastchanged = 'teadmata ajal'
+    
+#mailsender("kadijairus@gmail.com", "Üritatud uuendada","Üritatud uuendada dir-ist")
+
+allsheetnames = ["P","T","H","K","E","M"]
+
+names = pd.read_excel(dir_new,sheet_name='Töötajad',header=2,usecols=[4,18])
+names.columns = ['Nimi', 'Dpt']
+names.dropna()
+names = names[names['Dpt'].str.contains("kliiniline geneetika")==True]
+names = names['Nimi']
+#names = list(names["Name"])
+print(names)
+
+#mailsender("kadijairus@gmail.com", "Nimed laetud","Nimed laetud")
+
+
+def sheetcomparer(dir_old,dir_new,sheetname):
+    df_old = pd.read_excel(dir_old,sheet_name=sheetname,header=1,usecols=["Nimi","Algus","Lõpp"])
+    df_new = pd.read_excel(dir_new,sheet_name=sheetname,header=1,usecols=["Nimi","Algus","Lõpp"])
+    df = df_new.compare(df_old, align_axis=0, keep_shape = False).rename(index={'self': 'uus', 'other': 'enne muutmist'},level=-1)
+    df["Leht"] = sheetname
+    df.fillna('-')
+    return(df)
+
+df = []
+try:
+    df_appended=[]
+    if len(allsheetnames) > 0:
+        for asheetname in allsheetnames:
+            print(asheetname)
+            df_sheet = sheetcomparer(dir_old,dir_new,asheetname)
+            df_appended.append(df_sheet)
+            print(df_appended)
+        df = pd.concat(df_appended)
+        df['Algus'] = pd.to_datetime(df['Algus'])
+        df['Algus'] = df['Algus'].dt.strftime('%d.%m.%Y')
+        df['Lõpp'] = pd.to_datetime(df['Lõpp'])
+        df['Lõpp'] = df['Lõpp'].dt.strftime('%d.%m.%Y')
+        print('Enne filtreerimist')
+        print(df)
+        #df['Name'] =df['Nimi']
+        nimedefilter = (df['Nimi'].isin(names))
+        df = df.loc[nimedefilter]
+#        df = df.to_string(index=False)
+        print("FILTREERITUD")
+        print(df)
+    else:
+        print("No sheetnames!")
+except Exception as e:
+    print("Sheetcomparer error!")
+    print(str(e))
+
+#mailsender("kadijairus@gmail.com", "Df tehtud","Df tehtud")
+
+
+if len(df) > 0:
+    subject = "on tehtud muudatusi"
+else:
+    subject = "ei ole ühtegi muudatust"
+    df = ''
+#print(df)
+
+#teesiinpaus = 1/0
+
+#shutil.copy(dir_new,dir_old)
+    
+#mailsender("kadijairus@gmail.com", "len Df tehtud","len Df tehtud")
+
+
+
 #message1 = "Põhitekst asub siin"
 #df = df.encode('utf-8').decode('utf-8')
 #print(message1)
 #paus = 1/0
-mailsender("kadijairus@gmail.com", subject,df)
+outlookmailsender("kadi.jairus@kliinikum.ee", subject,df,lastchanged)
+outlookmailsender("kadijairus@gmail.com", subject,df,lastchanged)
+#mailsender("kadijairus@gmail.com", 'Teate koopia',df)
 
 os.remove(dir_old)
 os.rename(dir_new,dir_old)
